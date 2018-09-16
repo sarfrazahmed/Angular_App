7a2be5e41caf76c83943f265975a8366c544be99
Sarfraz Ahmed <sarfraz.contact@gmail.com>
2018-09-16 11:54:39 +0530

    Applied styles and onclick functionality

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,17 +8,17 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
   heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() {
    }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
