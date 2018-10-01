628f3039419da78338db3e35daed9945970c2f27
Sarfraz Ahmed <sarfraz.contact@gmail.com>
2018-10-01 23:09:14 +0530

    Added http request calls to service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,17 +3,25 @@ import { Hero } from './heroes/hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+    private log(message: string) {
+      this.messageService.add(`HeroService: ${message}`);
+    }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
